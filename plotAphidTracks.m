0a53357d51b0c3165fbaa58ea04fd465ccb386bd
M Ulmer <mulmer@macalester.edu>
2017-05-12 22:19:21 -0500

    removed dead code, cleaned up comments

--- plotAphidTracks.m
+++ plotAphidTracks.m
@@ -1,25 +1,7 @@
-%
-% plotAphidTracks.m does exactly what it says on the tin. REMEMBER TO UNCOMMENT YOUR DATASET FOR THE FIRST RUN.
-% If you’re using same dataset you can comment that out once it’s in memory.
-% if it’s huge you don’t want to load it every time. So uncomment/type in the dataset you wanna pull from
-% (it should be actual aphid position data) set the experiment number (aka video number, for the extended
-% simulations this is usually run number and not exp number at all. (that is, it should go up to 100 for the
-% 100 run simulations)) Also set the frame you want to start from - remember you have to start from frame 2 if
-% you’re using the actual experimental data, because those don’t have a first frame. Check your axes are the
-% right dimensions (or wing it, that’s fine too -) and you’re good to go.
-%
-% If it’s not working, your expnum is probably set to an experiment that doesn’t turn up in the data you selected.
-% Look at your data to be sure. Or, it’s possible your axes are wrong for the size of the data. Almost all of the stuff
-% I generated or have been playing with is 20cm, so you want the axis([-.3 .3 -.3 .3]); but some might need the bigger
-% axes or it may be auto-configured to something zoomed on a smaller region.
-% %
+% plotAphidTracks.m shows the movment of aphids over time by plotting their
+% 'trails'. If you’re using same dataset you can comment that out once it’s in memory.
 
 %aphiddata = load('aphiddata.csv');
-%aphiddata = load('aphiddata.csv');
-%aphiddata = load('fullModelDataExp4.csv');
-%%aphiddata = load('fullNoInteractionDataExp4.csv');
-%aphiddata = load('noInteractionData.csv');
-%aphiddata = load('fullNoInteractionDataRun.csv');
 
 expnum = 1;
 framenum = 2; %starting frame, goes out numOfFrames frames from here
@@ -63,21 +45,10 @@ firstOccurenceEachAphid = firstOccurenceEachAphid(any(firstOccurenceEachAphid,2)
 figure
 scatter(firstOccurenceEachAphid(:, 2), firstOccurenceEachAphid(:, 3), 'filled'); %TODO: this will not account for points that show up in later frames but not in this one!
 
-% theta = linspace(0,2*pi,1000);
-% circle = .2*exp(1i*theta);
-
 rectangle('Position',[-.2, -.2, .4, .4],'Curvature',[1 1])
 axis([-.25 .25 -.25 .25]);
 axis off
 pbaspect([1 1 1])
-
-
-% hold on; plot(circle,'b-'); hold off;
-
-% and add text labels
-% for j = 1:numel(firstOccurenceEachAphid(:, 1))
-%     text(firstOccurenceEachAphid(j, 2), firstOccurenceEachAphid(j, 3), strcat( ' - ', int2str(firstOccurenceEachAphid(j, 1))));
-% end
 
  %drawing lines between appropriate points
  for x = 1:maxAphidNum
